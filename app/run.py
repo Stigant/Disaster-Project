889c5ecdc76f11f74f467086aeed4713b22c1e51
Liam Stigant <liamstigant96@gmail.com>
2021-03-28 17:52:28 +0100

    Load Model

--- app/run.py
+++ app/run.py
@@ -1,6 +1,9 @@
 import json
 import plotly
+import joblib
 import pandas as pd
+import re
+
 
 from nltk.stem import WordNetLemmatizer
 from nltk.tokenize import word_tokenize
@@ -8,41 +11,64 @@ from nltk.tokenize import word_tokenize
 from flask import Flask
 from flask import render_template, request, jsonify
 from plotly.graph_objs import Bar
-from sklearn.externals import joblib
 from sqlalchemy import create_engine
+
+from importlib import import_module
+
+import sys
+
+sys.path.append("../models")
+
 
 
 app = Flask(__name__)
 
+def load_data(database_filepath):
+    """Load pandas dataframe from SQL database"""
+    engine = create_engine('sqlite:///'+database_filepath)
+    df=pd.read_sql_table('DisasterTable',engine).set_index('id')
+    return df
+
 def tokenize(text):
-    tokens = word_tokenize(text)
+    """Clean and tokenize text, then lemmatize"""
+    #Clean+Tokenize
+    text = re.sub(r"[^a-zA-Z0-9]", " ", text.lower())
+    text.lower()
+    tokens=word_tokenize(text)
+
+    #Lemmatize
+    stopWords=stopwords.words('english')
     lemmatizer = WordNetLemmatizer()
+    tokens = [lemmatizer.lemmatize(word) for word in tokens if word not in stopWords]
+    return tokens
 
-    clean_tokens = []
-    for tok in tokens:
-        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
-        clean_tokens.append(clean_tok)
 
-    return clean_tokens
-
-# load data
-engine = create_engine('sqlite:///../data/YourDatabaseName.db')
-df = pd.read_sql_table('YourTableName', engine)
+df = load_data("../data/Disaster-Messages-Categories.db")
 
 # load model
-model = joblib.load("../models/your_model_name.pkl")
+def keep_message(X):
+    return  X['message']
+def keep_genres(X):
+    return X[['genre_social', 'genre_news']]
+def thresh_fun(z):
+    return max(min(0.5,2*z.mean()), 0.25)
+
+
+model = joblib.load("../models/classifier.pkl")
 
 
 # index webpage displays cool visuals and receives user input text for model
 @app.route('/')
-@app.route('/index')
+@app.route('/templates/master.html')
 def index():
-    
+
     # extract data needed for visuals
     # TODO: Below is an example - modify to extract data for your own visuals
-    genre_counts = df.groupby('genre').count()['message']
+    genre_counts = keep_genres(df)
+    genre_counts['genre_other']=~(df.any(axis=1))
+    genre_counts=genre_counts.sum().transpose()
     genre_names = list(genre_counts.index)
-    
+
     # create visuals
     # TODO: Below is an example - modify to create your own visuals
     graphs = [
@@ -65,11 +91,11 @@ def index():
             }
         }
     ]
-    
+
     # encode plotly graphs in JSON
     ids = ["graph-{}".format(i) for i, _ in enumerate(graphs)]
     graphJSON = json.dumps(graphs, cls=plotly.utils.PlotlyJSONEncoder)
-    
+
     # render web page with plotly graphs
     return render_template('master.html', ids=ids, graphJSON=graphJSON)
 
@@ -78,13 +104,13 @@ def index():
 @app.route('/go')
 def go():
     # save user input in query
-    query = request.args.get('query', '') 
+    query = request.args.get('query', '')
 
     # use model to predict classification for query
     classification_labels = model.predict([query])[0]
     classification_results = dict(zip(df.columns[4:], classification_labels))
 
-    # This will render the go.html Please see that file. 
+    # This will render the go.html Please see that file.
     return render_template(
         'go.html',
         query=query,
